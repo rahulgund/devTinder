04781e7ff7926ef792c7788fdafcf35e2f322e1c
rahulgund002@gmail.com <rahulgund@Rahuls-Air.bbrouter>
2024-10-02 12:32:43 +0530

    after long break

--- src/app.js
+++ src/app.js
@@ -1,30 +1,85 @@
 const express = require("express")
-const dbConnect = require("./config/db.js")
+const dbConnect = require("./config/db.js")  // Database config
 const app = express()
-const User = require("./models/user.js")
+const User = require("./models/user.js")     // User Schema
 
+// All middlewares
 app.use(express.json())
 
 app.post("/signup", async (req,res)=> {
-    console.log(req.body);
-    
     const user = new User(req.body)
-
+    
     try {
-        await user.save()
+        const xyz= await user.save()
         res.send("User added successfully")
-    } catch (error) {
-        res.status(400).send("Error saving user")
+    } 
+    catch (error) {
+        res.status(400).send("Error saving user" + error.message)
     }
     //res.send("req body testing")
     
 })
 
+// Get User Data for tinder feed
+app.get("/user", async (req,res)=> {
+    const userEmail = req.body.emailId
+
+    try {
+            const user = await User.findOne({emailId : userEmail})
+            res.send(user)
+    } 
+    catch (error) {
+            res.status(401).send("Something went wrong")
+    }
+})
+
+// Get all users for tinder feed
+app.get("/feed", async (req,res)=> {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(401).send("Something went wrong")
+    }
+})
+
+// Delete user
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId
+    try {
+        const user = await User.findByIdAndDelete(userId)
+        res.send("User deleted successfully")
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
+})
+
+// Upadte user info
+app.patch("/user", async(req,res)=> {
+    const userId = req.body._id
+    const data = req.body
+    
+    try {
+        const ALLOWED_UPDATES = ["firstName", "lastName", "age", "gender", "password"]
+        const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k))
+        if(!isUpdateAllowed){
+            throw new Error("Upadate not allowed")
+        }
+        await User.findByIdAndUpdate({_id:userId}, data, { runValidators: true})
+        res.send("user updated successfully")
+    } 
+    catch (error) {
+        res.status(401).send("Upadate Failed " + error.message)
+    }
+})
+
+//---------------------------------------------------------
+// Root path
 app.get("/", (req,res) => {
     res.send("hello dev tinder")
 })
 
-
+// Mongodb Database COnnection
 dbConnect()
 .then(()=> {
     console.log("Database connection successful...");
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,47 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
+
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minlength:4,
+        maxlength:20
     },
     lastName: 
     {
         type: String
     },
     emailId :{
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address" + value)
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        min:18
     },
     gender:  {
-        type: String
+        type: String,
+        validate(value) {
+            if(!["male","female","others"].includes(value)){
+                throw new Errror("Gener is not valid")
+            }
+        }
     }
-})
+}, {timestamps: true})
 
 const User = mongoose.model("User", userSchema)
 
